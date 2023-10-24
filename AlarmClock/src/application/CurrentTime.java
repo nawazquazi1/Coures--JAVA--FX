844cffb79dad7a4067452cbce5d3071ef4b187e8
nawaz <nawazquazi356@gmail.com>
2023-10-24 12:24:43 +0530

    Alarm Clock

---
+++ AlarmClock/src/application/CurrentTime.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CurrentTime {
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+	LocalDateTime now = LocalDateTime.now();
+	
+	public CurrentTime() {
+	}
+	
+	public String currentTime() {return dtf.format(now);}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
