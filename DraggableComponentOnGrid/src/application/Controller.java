1110f3faf03d5769584769378b2d8c4293015fcb
nawaz <nawazquazi356@gmail.com>
2023-10-24 12:51:46 +0530

    Draggable Component On Grid

---
+++ DraggableComponentOnGrid/src/application/Controller.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private int gridSize = 50;
+
+    @FXML
+    private AnchorPane pane;
+
+    private GridHandler backgroundGridHandler;
+    private DraggableMakerGrid draggableMakerGrid;
+    private DraggableMaker draggableMaker = new DraggableMaker();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        draggableMakerGrid = new DraggableMakerGrid(pane.getPrefWidth(), pane.getPrefHeight(), gridSize, pane);
+
+        backgroundGridHandler = new GridHandler(pane.getPrefWidth(), pane.getPrefHeight(), gridSize, pane);
+        backgroundGridHandler.updateGrid();
+
+        Component component = new Component(gridSize, 100, 100);
+        pane.getChildren().add(component.getRectangle());
+
+        draggableMakerGrid.makeDraggable(component);
+//        draggableMaker.makeDraggable(component.getRectangle());
+    }
+}
+
+
+
+
+
