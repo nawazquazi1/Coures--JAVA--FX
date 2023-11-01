8f56545766a7628554cf944df5c5c5ace795e844
nawaz <nawazquazi356@gmail.com>
2023-11-01 18:48:43 +0530

    Snake-0.1

---
+++ Snake-0.1/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
